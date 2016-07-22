ce2c792f6a713fd625aef27ee99559984fd355db
jack <jack.gammon@equinox.co.nz>
2016-07-22 16:13:17 +1200

    Added more calculator functions

--- calculator/WebContent/calculator.js
+++ calculator/WebContent/calculator.js
@@ -3,13 +3,13 @@
  */
 var calculator = (function(){
 	var OPERATOR_REGEX = /\+|-|\/|\*/;
-	
+
 	var returnObject = {};
 	var entry = [];
 	var maxLength = 8
 	var operandA = null;
 	var operator = null;
-	
+
 	returnObject.pressButton = function(value){
 		if(typeof value === 'number' && entry.length < maxLength){
 			entry.push(value);
@@ -17,7 +17,7 @@ var calculator = (function(){
 		} else{
 			// check for operator
 			if(OPERATOR_REGEX.test(value)){
-				// no previous value has been entered (i.e. first in a chain
+				// no previous value has been entered (i.e. first in a chain)
 				if(operandA === null){
 					operandA = entry.join('');
 					entry = [];
@@ -30,35 +30,53 @@ var calculator = (function(){
 					operator = value;
 					updateDisplay();
 				}
+			} else if(value == "AC"){
+				//clear everything
+				entry = [];
+				operandA = null;
+				operator = null;
 			}
-			
+
 		}
 		updateDisplay();
 	};
-	
+
+
 	
 	function calculate(){
-		return (Number(operandA) + Number(entry.join('')));
-	}
-	
-	
-	
-	function updateDisplay(){
-		if(entry.length > 0){
-			setDisplay(entry.join(''));
-		} else if(operandA === null){
-			setDisplay(0);
-		} else{
-			setDisplay(operandA);
+		switch(operator){
+		case "-":
+			return (Number(operandA) - Number(entry.join('')));
+		case "/":
+			return (Number(operandA) / Number(entry.join('')));
+		case "*":
+			return (Number(operandA) * Number(entry.join('')));
+		default:
+			return (Number(operandA) + Number(entry.join('')));
 		}
 		
-		
-		
 	}
-	
+
+
+
+	function updateDisplay(value){
+		if(typeof value === 'undefined'){
+			if(entry.length > 0){
+				setDisplay(entry.join(''));
+			} else if(operandA === null){
+				setDisplay(0);
+			} else{
+				setDisplay(operandA);
+			}		
+		}
+		else {
+			setDisplay(vale);
+		}
+	}
+
 	function setDisplay(value){
 		document.getElementById("calc-display").innerHTML = value;
 	}
-	
+
 	return returnObject;
 })();
