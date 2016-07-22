c763c5b4d32acfd66b784511d2566b3ad9358b6d
jack <jack.gammon@equinox.co.nz>
2016-07-22 16:02:59 +1200

    added the buttons, got some of the basics of the javascript working

--- calculator/WebContent/calculator.js
+++ calculator/WebContent/calculator.js
@@ -1,22 +1,64 @@
 /**
  * 
  */
-
 var calculator = (function(){
-	// locally scoped object
+	var OPERATOR_REGEX = /\+|-|\/|\*/;
+	
 	var returnObject = {};
+	var entry = [];
+	var maxLength = 8
+	var operandA = null;
+	var operator = null;
 	
-	
-	returnObject.testFunc = function(value){
-		return value + " public, yay!";
+	returnObject.pressButton = function(value){
+		if(typeof value === 'number' && entry.length < maxLength){
+			entry.push(value);
+			console.log(entry.join(''));
+		} else{
+			// check for operator
+			if(OPERATOR_REGEX.test(value)){
+				// no previous value has been entered (i.e. first in a chain
+				if(operandA === null){
+					operandA = entry.join('');
+					entry = [];
+					operator = value;
+				}
+				// previous value is present
+				else{
+					operandA = calculate();
+					entry = [];
+					operator = value;
+					updateDisplay();
+				}
+			}
+			
+		}
+		updateDisplay();
 	};
 	
 	
+	function calculate(){
+		return (Number(operandA) + Number(entry.join('')));
+	}
 	
-	// return to give everyone access to our public methods
+	
+	
+	function updateDisplay(){
+		if(entry.length > 0){
+			setDisplay(entry.join(''));
+		} else if(operandA === null){
+			setDisplay(0);
+		} else{
+			setDisplay(operandA);
+		}
+		
+		
+		
+	}
+	
+	function setDisplay(value){
+		document.getElementById("calc-display").innerHTML = value;
+	}
+	
 	return returnObject;
 })();
-
-function testFunction(){
-	
-}
